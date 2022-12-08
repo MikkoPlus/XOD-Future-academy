9f7f9008b6eb3756fe7ce5fe780114c674a954f8
Anatoliy Sharov <sharov.012@gmail.com>
2022-12-09 01:12:37 +0300

    add app to show towns-list and add to local storage

--- js/script.js
+++ js/script.js
@@ -2,6 +2,66 @@
 
 setTimeout(function(){
 	location.reload();
-}, 5000);
+}, 25000);
 
+window.addEventListener('DOMContentLoaded', () => {
 
+	function closeOnEscape (currentClass, targetClass) {
+		document.addEventListener('keydown',(e) => {
+			e.preventDefault();
+			if (e.keyCode === 27) {
+				currentClass.classList.remove(targetClass);
+			}
+		});
+	}
+
+	function setValueToLocaleStorage(key, value) {
+		localStorage.setItem(key, value);
+	}
+
+	
+	const townTrigger = document.querySelector('.header__town');
+
+	if (localStorage.getItem('town')) {
+		townTrigger.textContent = localStorage.getItem('town');
+	} else {
+		townTrigger.textContent = 'Нижний Новгород';
+		setValueToLocaleStorage('town', townTrigger.textContent);
+	}
+
+	function townListHandler () {
+		const townsList = document.querySelector('.towns'),
+			closeIcon = townsList.querySelector('.towns__close');	
+
+		function closeTownList () {
+		closeIcon.addEventListener('click', () => townsList.classList.remove('towns_visible'));
+		}
+
+		townTrigger.addEventListener('click', (e) => {
+			e.preventDefault();
+
+			townsList.classList.toggle('towns_visible');
+			if (townsList.classList.contains('towns_visible')) {
+				setTimeout(function() {
+					townsList.closest('.page').addEventListener('click', event => {
+						if (!event.target.closest('.header')) {
+							townsList.classList.remove('towns_visible');
+						}
+					});
+				}, 500);
+
+				townsList.addEventListener('click', event => {
+					if (event.target.classList.contains('towns__list-town')) {
+						const targetTown = event.target;
+
+						setValueToLocaleStorage('town', targetTown.textContent);
+						townTrigger.textContent = targetTown.textContent;
+					}
+				});
+				closeOnEscape(closeOnEscape, 'towns_visible');
+				closeTownList ();
+			}
+		});
+	}
+	townListHandler();
+});
